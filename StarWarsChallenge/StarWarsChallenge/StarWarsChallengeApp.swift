5e436b38b8e5289f93677c78664088a683fc0a03
DmytroSalenko <41685806+DmytroSalenko@users.noreply.github.com>
2023-03-29 13:26:20 -0400

    Project setup

---
+++ StarWarsChallenge/StarWarsChallenge/StarWarsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsChallengeApp.swift
+//  StarWarsChallenge
+//
+//  Created by Dima Salenko on 2023-03-29.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
